234d235a345e26d279eea0d5e40cd030579117d6
Lysquid <romainbenoit21560@gmail.com>
2024-04-12 16:00:50 +0200

    vscode: write what is written

--- plugins/vscode/smartshare/src/extension.ts
+++ plugins/vscode/smartshare/src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "smartshare" is now active!');
+    let event_sent = new Array();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('smartshare.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SmartShare!');
-	});
+	let disposable = vscode.workspace.onDidChangeTextDocument(event => {
 
-	context.subscriptions.push(disposable);
+        for (let change of event.contentChanges) {
+
+            if (event_sent[event_sent.length-1] == (change.range, change.text)) {
+                event_sent.pop();
+                vscode.window.showInformationMessage('Caught event');
+            } else {
+                console.log(change);
+
+                const editor = vscode.window.activeTextEditor;
+                if (editor) {
+                    editor.edit((editBuilder: vscode.TextEditorEdit) => {
+                        event_sent.push((change.range, change.text));
+                        editBuilder.replace(change.range, change.text);
+                    }).then(success => {
+                        if (success) {
+                            vscode.window.showInformationMessage('Text inserted');
+                        } else {
+                            vscode.window.showErrorMessage('Failed to insert text');
+                        }
+                    });
+                }
+            }
+
+        }
+    });
+
+    context.subscriptions.push(disposable);
+
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
