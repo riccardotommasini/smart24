9987c046143642c1eb59f27892c39d995cbca4fa
Charles-François St-Cyr <charlesf2001@gmail.com>
2024-04-28 12:42:55 +0200

    Update path mapping for 'common' module in jest.config.ts and tsconfig.json

--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -90,7 +90,6 @@ module.exports = {
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     moduleNameMapper: {
         '^common$': '<rootDir>/../../packages/common/src',
-        '^common/(.*)$': '<rootDir>/../../packages/common/src/$1',
     },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
