b189231b1b16c1db97db25c745faff577ac6f81b
Alix Peigue <irobot.moi@gmail.com>
2024-04-29 11:58:11 +0200

    client

--- smartshare/src/client/main.rs
+++ smartshare/src/client/main.rs
@@ -1,4 +1,29 @@
+use core::panic;
+use std::env;
+
+use futures::{try_join, SinkExt};
+use smartshare::protocol::{message_sink, message_stream};
+use tokio::net::TcpStream;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello from client");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Usage : {} ip:port", args[0]);
+    }
+
+    let mut stdin_stream = message_stream(tokio::io::stdin());
+    let mut stdout_sink = message_sink(tokio::io::stdout());
+    let mut binding = TcpStream::connect(args[1].clone()).await.unwrap();
+    let (rx, tx) = binding.split();
+    let mut tcp_stream = message_stream(rx);
+    let mut tcp_sink = message_sink(tx);
+
+    // tokio::spawn(async move { stdout_sink.send_all(&mut tcp_stream).await.unwrap() });
+    // tcp_sink.send_all(&mut stdin_stream).await.unwrap();
+    try_join!(
+        stdout_sink.send_all(&mut tcp_stream),
+        tcp_sink.send_all(&mut stdin_stream)
+    )
+    .unwrap();
 }
