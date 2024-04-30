5305f3ab5cc4c9ae29e7dc309c0bd07faa8ef2c2
tao97410 <tao.mouysset@insa-lyon.fr>
2024-05-01 00:57:53 +0200

    front-end register login

--- apps/client/src/components/registerForm.vue
+++ apps/client/src/components/registerForm.vue
@@ -1,23 +1,24 @@
 <script>
-    import '../assets/HomeView.css'
+    import '../assets/RegisterView.css'
 </script>
 
 <template>
 
     <div class="container">
         <form class="login-form">
+            <input type="text" class="input-field" placeholder="Pseudo">
             <input type="email" class="input-field" placeholder="Email">
             <input type="password" class="input-field" placeholder="Password">
-            <button type="submit" class="login-button">Login</button>
-            <a class="forgot-password" href="">Forgot password ?</a>
+            <input type="password" class="input-field" placeholder="Confirm password">
             <div class="line-container">
                 <div class="line"></div>
-                <div class="or">OR</div>
+                <div class="circle"></div>
                 <div class="line"></div>
             </div>
+            <button type="submit" class="login-button">Create an account</button>
+            
 
             
-            <button type="button" class="login-button">Create an account</button>
         </form>
     </div>
 
---
+++ apps/client/src/views/RegisterView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import slogan from '../components/slogan.vue';
+import registerForm from '../components/registerForm.vue';
+import '../assets/HomeView.css'
+
+</script>
+
+<template>
+  
+  <slogan/>
+  <registerForm/>
+
+</template>
+
+<style scoped>
+
+</style>
+  
+ 
+  
+  
+  
