0b9c16bc91af46b6669848d4e2aafaaa02f57000
totoletoto69 <thomas.boyer69100@gmail.com>
2024-04-30 08:28:22 +0200

    userts

--- apps/server/src/models/user.ts
+++ apps/server/src/models/user.ts
@@ -9,28 +9,30 @@ interface IUser extends Document {
     mail: string,
     username: string,
     passwordHash: string,
-    globalTrust: boolean,
     factChecker: boolean,
     posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
     follows: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     subscribers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     trustedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     distrustedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    parameters: []
+    parameters: {globalTrust: boolean, rateFactChecked: number, diversification: number}
 }
 
 const UserSchema: Schema = new Schema<IUser>({
     userId: { type: Schema.Types.ObjectId },
-    username: { type: String, required: true, maxLength: 100, unique: true },
+    name: {type: String, required: true, maxlength: 100},
+    surname: {type: String, required: true, maxlength: 100},
+    birthday: {type: DateTime, required: true},
     mail: { type: String, required: true, maxLength: 100, unique: true },
+    username: { type: String, required: true, maxLength: 100, unique: true },
     passwordHash: { type: String, required: true, maxLength: 256 },
-    globalTrust: { type: Boolean, default: false },
     factChecker: { type: Boolean, default: false },
     posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
     follows: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     subscribers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     trustedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     distrustedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    parameters: {globalTrust: {type: Boolean, required: true, default: false}, rateFactChecked: {type: Number, }}
 });
 
 // Export model
