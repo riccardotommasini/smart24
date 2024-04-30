3f5219820831dfdcce4920c008d0ae93328c79d9
totoletoto69 <thomas.boyer69100@gmail.com>
2024-04-30 16:29:22 +0200

    auth

--- apps/server/src/middleware/auth.ts
+++ apps/server/src/middleware/auth.ts
@@ -1,9 +1,13 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { env } from '../utils/env';
+import { HttpException } from '../models/http-exception';
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/user';
 
 export interface CustomRequest extends Request {
     token: string | JwtPayload;
+    username: string;
 }
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
@@ -11,11 +15,18 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
         const token = req.header('Authorization')?.replace('Bearer ', '');
 
         if (!token) {
-            throw new Error();
+            throw new HttpException(StatusCodes.UNAUTHORIZED, 'You must be logged in');
         }
 
-        const decoded = jwt.verify(token, env.SECRET_KEY);
+        const decoded = jwt.verify(token, env.SECRET_KEY) as { username: string };
+
+        const foundUser = await User.findOne({ username: decoded.username });
+
+        if (!foundUser) {
+            throw new HttpException(StatusCodes.UNAUTHORIZED, 'User not found');
+        }
         (req as CustomRequest).token = decoded;
+        (req as CustomRequest).username = decoded.username;
 
         next();
     } catch (err) {
