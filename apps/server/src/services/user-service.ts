a31f51b93f236af0c80b66eb57a3581346418ba0
martinlecluse <leclusemartin@gmail.com>
2024-04-30 16:49:53 +0200

    correct merge

--- apps/server/src/services/user-service.ts
+++ apps/server/src/services/user-service.ts
@@ -35,41 +35,6 @@ export class UserService {
         }
     }
 
-    public user_create_post: RequestHandler[] = [
-        // Validate and sanitize fields.
-        body('username', 'Username required').trim().isLength({ min: 1 }).escape(),
-        body('mail', 'E-mail required').trim().isLength({ min: 1 }).escape(),
-        body('password', 'Password required').trim().isLength({ min: 1 }).escape(),
-        // Process request after validation and sanitization.
-
-        asyncHandler(async (req: Request, res: Response) => {
-            // Extract the validation errors from a request.
-            const errors = validationResult(req);
-
-            const passwordHash = crypto.createHash('sha256').update(req.body.password).digest('hex');
-            
-            // Create a Book object with escaped and trimmed data.
-            const user = new User({
-                username: req.body.username,
-                mail: req.body.mail,
-                passwordHash: passwordHash,
-            });
-
-            if (!errors.isEmpty()) {
-                // There are errors. Render form again with sanitized values/error messages.
-            } else {
-                // Data from form is valid. Save book.
-                try {
-                    await user.save();
-                    res.status(StatusCodes.OK).send('user saved !!!');
-                } catch (error) {
-                    console.error(error);
-                    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Error saving user');
-                }
-            }
-        }),
-    ];
-
     /*
     Test :
     curl -X POST -H "Content-Type: application/json" -d '{"username":"momo","mail":"a@gmail.com","password":"azerty"}' http://localhost:3000/user/create
@@ -160,6 +125,7 @@ export class UserService {
             
                     return response;
             });
+    }
       
     async saveUser(
         username: string,
