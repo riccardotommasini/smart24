96317c4ef61cc380f0a97e20404412465dadf122
pvelosa <paula.velosa2704@gmail.com>
2024-04-30 09:43:50 +0200

    frontend setup

--- apps/client/src/components/slogan.vue
+++ apps/client/src/components/slogan.vue
@@ -1,13 +1,12 @@
-<template>
-
-    <div class="slogan">
-      <h1 class="div-h1 laila">Authentica</h1>
-      <p class="div-p laila">Where truth finds its <br> voice.</p>
-    </div> 
-
-  </template>
-
-
-  <script>
+<script>
   import '../assets/HomeView.css'
 </script>
+
+<template>
+
+  <div class="slogan">
+    <h1 class="div-h1 laila">Authentica</h1>
+    <p class="div-p laila">Where truth finds its <br> voice.</p>
+  </div> 
+
+</template>
