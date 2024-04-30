c972fe7f1fd9b31b51079a3c05b9050342a85841
Lea-drd <73934530+Lea-drd@users.noreply.github.com>
2024-04-30 18:07:28 +0200

    add evolution mer de glace + selection front

--- glaciers_vulgarisation/api/static/js/script.js
+++ glaciers_vulgarisation/api/static/js/script.js
@@ -1,6 +1,46 @@
 function toggleMenu() {
     var menu = document.getElementById("menu");
-    var container = document.getElementById("parts-container");
     menu.classList.toggle("active");
-    container.classList.toggle("menu-open");
 }
+
+// Récupérer les éléments de l'animation container
+const evolutionContainer1 = document.getElementById("evolution-container-1");
+const evolutionContainer2 = document.getElementById("evolution-container-2");
+const evolutionContainer3 = document.getElementById("evolution-container-3");
+const evolutionContainer4 = document.getElementById("evolution-container-4");
+
+// Récupérer les éléments d'options de température
+const temperatureOptions = document.querySelectorAll(".temperature-option input[type='radio']");
+
+// Fonction pour afficher l'animation correspondante à l'option sélectionnée
+function afficherAnimation() {
+    if (this.value === "1.5") {
+        evolutionContainer1.style.display = "block";
+        evolutionContainer2.style.display = "none";
+        evolutionContainer3.style.display = "none";
+        evolutionContainer4.style.display = "none";
+    } else if (this.value === "2") {
+        evolutionContainer1.style.display = "none";
+        evolutionContainer2.style.display = "block";
+        evolutionContainer3.style.display = "none";
+        evolutionContainer4.style.display = "none";
+    } else if (this.value === "3") {
+        evolutionContainer1.style.display = "none";
+        evolutionContainer2.style.display = "none";
+        evolutionContainer3.style.display = "block";
+        evolutionContainer4.style.display = "none";
+    }else if (this.value === "4") {
+        evolutionContainer1.style.display = "none";
+        evolutionContainer2.style.display = "none";
+        evolutionContainer3.style.display = "none";
+        evolutionContainer4.style.display = "block";
+    }
+}
+
+// Ajouter des écouteurs d'événements sur les options de température
+temperatureOptions.forEach(option => {
+    option.addEventListener("change", afficherAnimation);
+});
+
+// Exécuter la fonction initiale pour afficher l'animation par défaut
+afficherAnimation.call(document.querySelector('.temperature-option input:checked'));
