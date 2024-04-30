2c400c6100e3e00f6769ece2079b26a7961dbfea
pvelosa <paula.velosa2704@gmail.com>
2024-04-30 10:37:29 +0200

    set up windows

--- apps/client/src/App.vue
+++ apps/client/src/App.vue
@@ -1,30 +1,12 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
 
-const serverMessage = ref('');
-
-axios.get('/').then((response) => {
-  serverMessage.value = response.data;
-});
-
-</script>
 
 <template>
   <header>
     <header>
-      <HelloWorld :msg="serverMessage" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      "hjhk"
     </header>
   </header>
 
-  <RouterView />
 </template>
 
 <style scoped>
