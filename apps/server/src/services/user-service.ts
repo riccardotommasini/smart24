6c7a01e58d47aff1ad14451da499bab08723dda7
totoletoto69 <thomas.boyer69100@gmail.com>
2024-04-30 16:17:45 +0200

    fixed date splitting

--- apps/server/src/services/user-service.ts
+++ apps/server/src/services/user-service.ts
@@ -1,9 +1,6 @@
 import { singleton } from 'tsyringe';
 import { DatabaseService } from './database-service/database-service';
 import { Document } from 'mongodb';
-import { Request, Response, RequestHandler } from 'express';
-import asyncHandler from 'express-async-handler';
-import { body, validationResult } from 'express-validator';
 import User from '../models/user';
 import { StatusCodes } from 'http-status-codes';
 import { DocumentDefinition } from 'mongoose';
@@ -16,49 +13,6 @@ import { HttpException } from '../models/http-exception';
 @singleton()
 export class UserService {
     constructor(private readonly databaseService: DatabaseService) {}
-
-    getMessage() {
-        return 'Hello world! ';
-    }
-
-    pingDb(): Promise<Document> {
-        return this.databaseService.client.db('admin').command({ ping: 1 });
-    }
-
-    public user_create_post: RequestHandler[] = [
-        // Validate and sanitize fields.
-        body('username', 'Title must not be empty.').trim().isLength({ min: 1 }).escape(),
-        body('mail', 'Author must not be empty.').trim().isLength({ min: 1 }).escape(),
-        body('password', 'Summary must not be empty.').trim().isLength({ min: 1 }).escape(),
-        // Process request after validation and sanitization.
-
-        asyncHandler(async (req: Request, res: Response) => {
-            // Extract the validation errors from a request.
-            const errors = validationResult(req);
-
-            // const passwordHash = crypto.createHash('sha256').update(req.body.password).digest('hex');
-            // http://localhost:8888/user/create?username=momo&mail=a@gmail.com&password=azerty
-            // Create a Book object with escaped and trimmed data.
-            const user = new User({
-                username: req.body.username,
-                mail: req.body.mail,
-                passwordHash: req.body.password,
-            });
-
-            if (!errors.isEmpty()) {
-                // There are errors. Render form again with sanitized values/error messages.
-            } else {
-                // Data from form is valid. Save book.
-                try {
-                    await user.save();
-                    res.status(StatusCodes.OK).send('user saved !!!');
-                } catch (error) {
-                    console.error(error);
-                    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Error saving user');
-                }
-            }
-        }),
-    ];
 
     public async login(user: DocumentDefinition<IUser>) {
         const foundUser = await UserSchema.findOne({ username: user.username });
@@ -102,15 +56,18 @@ export class UserService {
         }
 
         // const passwordHash = crypto.createHash('sha256').update(password).digest('hex');
-        const [day, month, year] = birthday.split('/');
-        const birthdayDate = new Date(Number(year), Number(month) - 1, Number(day));
+        let birthdayDate: Date;
+        if (birthday) {
+            const [day, month, year] = birthday.split('/');
+            birthdayDate = new Date(Number(year), Number(month) - 1, Number(day));
+        }
         const user = new User({
             username: username,
             mail: mail,
             passwordHash: password,
             name: name,
             surname: surname,
-            birthday: birthdayDate,
+            birthday: birthdayDate!,
             factChecker: factChecker,
             organization: organization,
         });
