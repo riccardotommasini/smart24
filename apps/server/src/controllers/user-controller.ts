ed773b4c7eda3ee912e421f5e21a6eadf6cbd859
Mohamed Fayala <fayalla.mohamed@gmail.com>
2024-04-30 12:02:48 +0200

    fix

--- apps/server/src/controllers/user-controller.ts
+++ apps/server/src/controllers/user-controller.ts
@@ -1,12 +1,9 @@
-import crypto from 'crypto';
-import { Request, Response, Router, RequestHandler } from 'express';
-import asyncHandler from 'express-async-handler';
-import { body, validationResult } from 'express-validator';
+import { Router } from 'express';
 import { singleton } from 'tsyringe';
-import User from '../models/user';
 import { AbstractController } from './abstract-controller';
-import { StatusCodes } from 'http-status-codes';
 import { UserService } from '../services/user-service';
+import { StatusCodes } from 'http-status-codes';
+import { body } from 'express-validator';
 
 @singleton()
 export class UserController extends AbstractController {
@@ -15,8 +12,13 @@ export class UserController extends AbstractController {
     }
 
     protected configureRoutes(router: Router) {
-        router.post('/user/create', (req, res, next) => {
-            this.userService.user_create_post.forEach((handler) => handler(req, res, next));
+        router.post('/user/create', async (req, res, next) => {
+            try {
+                await this.userService.saveUser(req.body.username, req.body.mail, req.body.password);
+                res.status(StatusCodes.OK).send('Connected to db!');
+            } catch (e) {
+                next(e);
+            }
         });
     }
 }
