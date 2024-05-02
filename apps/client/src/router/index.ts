a1f71f9887100d05e308c6945c4d11e47431e8c8
Charles-François St-Cyr <charlesf2001@gmail.com>
2024-05-02 10:07:08 +0200

    private routes

--- apps/client/src/router/index.ts
+++ apps/client/src/router/index.ts
@@ -4,6 +4,7 @@ import PageAccueil from '../views/PageAccueil.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import UserProfileView from '@/views/UserProfileView.vue'
 import Feed from '../components/common/feed.vue'
+import { useTokenStore } from '@/stores/auth'
 
 
 const router = createRouter({
@@ -12,28 +13,56 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        isPublic: true,
+      },
     },
     {
       path: '/homepage',
       name: 'homepage',
-      component : PageAccueil
+      component : PageAccueil,
+      meta: {
+        isPrivate: true,
+      },
     },
     {
       path: '/register',
       name: 'register',
-      component: RegisterView
+      component: RegisterView,
+      meta: {
+        isPublic: true,
+      },
     },
     {
       path: '/profile',
       name: 'profile',
-      component: UserProfileView
+      component: UserProfileView,
+      meta: {
+        isPrivate: true,
+      },
     },{
       path: '/feed',
       name: 'feed',
-      component: Feed
+      component: Feed,
+      meta: {
+        isPrivate: true,
+      },
+    },{
+      path: '/:pathMatch(.*)*',
+      redirect: { name: 'home' },
     }
   ]
+});
+
+router.beforeEach((to, from) => {
+  const tokenStore = useTokenStore();
+
+  if (to.meta.isPrivate && !tokenStore.isLoggedIn()) {
+    return { name: 'home' };
+  } else if (to.meta.isPublic && tokenStore.isLoggedIn()) {
+    return { name: 'homepage' };
+  }
 })
 
 export default router
